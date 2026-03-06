951e07b70ac3ee8c21f63416bd739df85c621733
done-4u <44857899+done-4u@users.noreply.github.com>
2026-03-06 16:57:12 +0900

    chore(footer): move footer into separated file

--- js/include.js
+++ js/include.js
@@ -3,3 +3,9 @@ fetch('navbar.html')
     .then(data => {
         document.getElementById("navbar-container").innerHTML = data;
     });
+
+fetch('footer.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById("footer-container").innerHTML = data;
+    });
