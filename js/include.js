e9b132a68a3cddf31ecd0564615c2b224d88f93b
done-4u <44857899+done-4u@users.noreply.github.com>
2026-03-10 03:28:44 +0900

    fix(include): prevent fetching if no footer

--- js/include.js
+++ js/include.js
@@ -1,16 +1,18 @@
-fetch("is-signed-in", {credentials: "include"})
+// navbar is in all pages
+fetch("is-signed-in", {credentials: "include", headers: {"Accept": "application/json"}})
     .then(response => response.json())
-    .then(data => data.isSignedIn)
-    .then(isSignedIn => {
-        fetch(isSignedIn ? "navbar-user.html" : "navbar-guest.html")
-            .then(response => response.text())
-            .then(data => {
-                document.getElementById("navbar-container").innerHTML = data;
-            });
-    })
+    .then(data => fetch(data.isSignedIn ? "navbar-user.html" : "navbar-guest.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("navbar-container").innerHTML = data;
+        }))
 
-fetch('footer.html')
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById("footer-container").innerHTML = data;
-    });
+// some pages do not contain footer
+const footer_container = document.getElementById("footer-container")
+if (footer_container) {
+    fetch('footer.html')
+        .then(response => response.text())
+        .then(data => {
+            footer_container.innerHTML = data;
+        });
+}
