c2cfc6c1ce3c0da6008a16b56bc87d57ecb7a23e
done-4u <44857899+done-4u@users.noreply.github.com>
2026-03-17 04:27:34 +0900

    feat: use announcement-list-common.html

--- common/include.js
+++ common/include.js
@@ -1,6 +1,18 @@
-// trick: there is navbar.html in user directory as well as guest directory
-fetch("navbar.html")
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById("navbar-container").innerHTML = data;
-    })
+function insert(input_file, parent_id) {
+    const parent = document.getElementById(parent_id);
+    if (parent) {
+        fetch(input_file).then(response => response.text()).then(data => {
+            const fragment = document.createRange().createContextualFragment(data)
+            parent.appendChild(fragment)
+        })
+    }
+}
+
+const PAIRS = [// trick: there is navbar.html in user directory as well as guest directory
+    ["navbar.html", "navbar-container"],
+
+    ["../common/announcement-list-common.html", "announcement-list-container"]]
+
+for (const pair of PAIRS) {
+    insert(pair[0], pair[1])
+}
