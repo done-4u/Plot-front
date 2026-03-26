ddfc04341ea4f4caebcebc2e27d2b91aed9c448d
done-4u <44857899+done-4u@users.noreply.github.com>
2026-03-26 19:12:12 +0900

    chore(navbar): rename navbar script

--- common/navbar-global.js
+++ common/navbar-global.js
@@ -2,7 +2,7 @@ fetch("/api/signed-in-as")
     .then(response => response.json())
     .then(data => {
         // (probably) navbar is anywhere, so parent is defined
-        const parent = document.getElementById("navbar-container");
+        const parent = document.getElementById("navbar-global-container");
         const username = data.username
 
         // API: the username is empty string("") if not signed in
