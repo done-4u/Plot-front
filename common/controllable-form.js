3c5ee21457e465241da15a290e2d7df3c4586b33
done-4u <44857899+done-4u@users.noreply.github.com>
2026-03-31 05:11:21 +0900

    chore: change custom element name

---
+++ common/controllable-form.js
@@ -0,0 +1,85 @@
+class ControllableForm extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    static get observedAttributes() {
+        return ["inner-value", "inner-required"];
+    }
+
+    get innerValue() {
+        const input = this.getElementsByTagName("input")[0];
+        return input ? input.value : this.getAttribute("inner-value") || "";
+    }
+
+    set innerValue(newValue) {
+        this.setAttribute("inner-value", newValue);
+    }
+
+    connectedCallback() {
+        this.render();
+        this.setupListeners();
+        this.syncAttributes();
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        const input = this.getElementsByTagName("input")[0];
+        if (!input) {
+            return;
+        }
+
+        if (name === "inner-value") {
+            input.value = newValue;
+        }
+
+        if (name === "inner-required") {
+            input.required = this.hasAttribute("inner-required");
+        }
+    }
+
+    syncAttributes() {
+        const input = this.querySelector("input");
+        if (input) {
+            input.value = this.getAttribute("inner-value") || "";
+            input.required = this.hasAttribute("inner-required");
+        }
+    }
+
+    render() {
+        const initialValue = this.getAttribute("inner-value") || "";
+        const initialRequired = this.hasAttribute("inner-required") ? "required" : "";
+
+        this.innerHTML = `
+            <form>
+                <input type="text" value="${initialValue} ${initialRequired}" disabled>
+                <button type="button" class="edit-button">Edit</button>
+                <button type="submit" class="submit-button" style="display: none;">Submit</button>
+            </form>
+        `;
+    }
+
+    setupListeners() {
+        const form = this.getElementsByTagName("form")[0];
+        const input = this.getElementsByTagName("input")[0];
+        const edit_button = this.getElementsByClassName("edit-button")[0];
+        const submit_button = this.getElementsByClassName("submit-button")[0];
+
+        edit_button.addEventListener("click", () => {
+            input.disabled = false;
+            input.focus();
+            edit_button.style.display = "none";
+            submit_button.style.display = "";
+        });
+
+        form.addEventListener("submit", (e) => {
+            e.preventDefault();
+
+            input.disabled = true;
+            submit_button.style.display = "none";
+            edit_button.style.display = "";
+            this.setAttribute("inner-value", input.value);
+        });
+    }
+}
+
+customElements.define("controllable-form", ControllableForm);
